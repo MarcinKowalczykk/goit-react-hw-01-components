b3f89de27e952cb3017e2322f8603c9783225772
Marcin Kowalczyk <marcinck80@gmail.com>
2023-10-14 15:12:07 +0200

    fixing-repo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import Profile from "./Profile/Profile";
-import Statistics from "./Statistics/Statistics";
+import Profile from "./profile/Profile";
+import Statistics from "./statistics/Statistics";
 import FriendList from "./FriendList/FriendList";
 
 import user from 'user';
